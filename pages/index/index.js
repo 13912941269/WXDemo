5ea078748a2eea3516aaa7f9fa401a28c8d4a025
13912941269@163.com <13912941269@163.com>
2018-03-26 13:18:26 +0800

    提交文件

---
+++ pages/index/index.js
@@ -0,0 +1,40 @@
+//index.js
+var app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      {
+        link: '/pages/index/index',
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
+      }, {
+        link: '/pages/logs/logs',
+        url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg'
+      }, {
+        link: '/pages/test/test',
+        url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      }
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    userInfo: {}
+  },
+  onLoad: function () {
+    var that=this;
+    wx.request({
+      url: 'http://118.25.15.253/HeNuo/mobile/alluserinfo.do', //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data.body);
+        that.setData({
+          userlist: res.data.body
+        })
+      }
+    })
+  }
+})  
+
